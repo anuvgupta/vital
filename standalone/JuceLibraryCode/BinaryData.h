191a0ea4c98cae1244e07ad5e599eb4e4de3aa46
Anuv Gupta <anuv.ca@gmail.com>
2026-02-09 02:22:31 -0800

    Add build files on windows

--- standalone/JuceLibraryCode/BinaryData.h
+++ standalone/JuceLibraryCode/BinaryData.h
@@ -9,13 +9,13 @@
 namespace BinaryData
 {
     extern const char*   _5_Limit_scl;
-    const int            _5_Limit_sclSize = 87;
+    const int            _5_Limit_sclSize = 102;
 
     extern const char*   _7_Limit_scl;
-    const int            _7_Limit_sclSize = 85;
+    const int            _7_Limit_sclSize = 100;
 
     extern const char*   Pythagorean_scl;
-    const int            Pythagorean_sclSize = 108;
+    const int            Pythagorean_sclSize = 123;
 
     extern const char*   default_vitalskin;
     const int            default_vitalskinSize = 10485;
@@ -45,31 +45,31 @@ namespace BinaryData
     const int            MontserratRegular_ttfSize = 245708;
 
     extern const char*   chorus_svg;
-    const int            chorus_svgSize = 12842;
+    const int            chorus_svgSize = 12844;
 
     extern const char*   cog_svg;
     const int            cog_svgSize = 472;
 
     extern const char*   compressor_svg;
-    const int            compressor_svgSize = 3175;
+    const int            compressor_svgSize = 3177;
 
     extern const char*   delay_svg;
-    const int            delay_svgSize = 3334;
+    const int            delay_svgSize = 3336;
 
     extern const char*   distortion_svg;
-    const int            distortion_svgSize = 2015;
+    const int            distortion_svgSize = 2017;
 
     extern const char*   drag_drop_arrows_svg;
-    const int            drag_drop_arrows_svgSize = 3810;
+    const int            drag_drop_arrows_svgSize = 3812;
 
     extern const char*   effects_filter_svg;
-    const int            effects_filter_svgSize = 1495;
+    const int            effects_filter_svgSize = 1497;
 
     extern const char*   equalizer_svg;
-    const int            equalizer_svgSize = 2816;
+    const int            equalizer_svgSize = 2818;
 
     extern const char*   flanger_svg;
-    const int            flanger_svgSize = 3334;
+    const int            flanger_svgSize = 3336;
 
     extern const char*   folder_svg;
     const int            folder_svgSize = 154;
@@ -78,19 +78,19 @@ namespace BinaryData
     const int            link_svgSize = 317;
 
     extern const char*   phaser_svg;
-    const int            phaser_svgSize = 1588;
+    const int            phaser_svgSize = 1590;
 
     extern const char*   reverb_svg;
-    const int            reverb_svgSize = 14394;
+    const int            reverb_svgSize = 14396;
 
     extern const char*   shuffle_svg;
     const int            shuffle_svgSize = 382;
 
     extern const char*   vital_ring_svg;
-    const int            vital_ring_svgSize = 2658;
+    const int            vital_ring_svgSize = 2660;
 
     extern const char*   vital_v_svg;
-    const int            vital_v_svgSize = 2658;
+    const int            vital_v_svgSize = 2660;
 
     extern const char*   vital_word_svg;
     const int            vital_word_svgSize = 3979;
