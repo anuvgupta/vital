01afda6666129e8565cd6dece902eaf1b6804018
Anuv Gupta <anuv.ca@gmail.com>
2026-02-22 23:14:42 -0800

    Update x icon for cancel-edit button

--- src/interface/look_and_feel/paths.h
+++ src/interface/look_and_feel/paths.h
@@ -1041,9 +1041,9 @@ class Paths {
     static Path cancelEditXIcon() {
       static const char svg[] =
         "<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\">"
-        "<path d=\"M18.7 5.3a1.2 1.2 0 0 0-1.7 0L12 10.3 7 5.3a1.2 1.2 0 0 0-1.7 1.7"
-        "L10.3 12 5.3 17a1.2 1.2 0 0 0 1.7 1.7L12 13.7l5 5a1.2 1.2 0 0 0 1.7-1.7"
-        "L13.7 12l5-5a1.2 1.2 0 0 0 0-1.7z\" fill=\"#ffffff\"/>"
+        "<path d=\"M18.7 5.3a1.5 1.5 0 0 0-2.12 0L12 9.88 7.42 5.3a1.5 1.5 0 0 0-2.12 2.12"
+        "L9.88 12 5.3 16.58a1.5 1.5 0 0 0 2.12 2.12L12 14.12l4.58 4.58a1.5 1.5 0 0 0 2.12-2.12"
+        "L14.12 12l4.58-4.58a1.5 1.5 0 0 0 0-2.12z\" fill=\"#ffffff\"/>"
         "</svg>";
       Path path = fromSvgData(svg, sizeof(svg) - 1);
       path.addLineSegment(Line<float>(0.0f, 0.0f, 0.0f, 0.0f), 0.01f);
